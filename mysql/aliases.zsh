d3b418d84751e38236d2c53e140be12f5592fffe
Frank van Viegen <frank@paiq.nl>
2013-08-13 18:11:43 +0200

    Updated git-perl

--- mysql/aliases.zsh
+++ mysql/aliases.zsh
@@ -1,10 +1,7 @@
-alias m="mysql -D paiq0000 -h heat-int --port=3306 -u paiq0000 -p"
-alias mrepl="mysql -D paiq0000 -h snatch-int -u paiq0000 -p"
-alias mtest="mysql -D paiqtest -h snatch-int -u test -p"
-
-alias mtj="mysql -D tjetter -h heat-int --port=3306 -u tjetter -p"
-alias msandtj="mysql -h memento-int -u tjetter_sandbox -D tjetter_sandbox -p"
-alias miml="mysql -D iml -h snatch-int -u iml -p"
+alias mprod="mysql -D paiq0000 -h heat-int --port=3306 -u paiq0000 -p"
+alias mrepl1="mysql -D paiq0000 -h mib-int --port=3306 -u paiq0000 -p"
+alias mrepl2="mysql -D paiq0000 -h snatch-int --port=3307 -u paiq0000 -p"
+alias mtest="mysql -D paiqtest -h snatch-int --port=3306 -u test -p"
 
 export MYSQL_PS1="\u@\h > "
 
